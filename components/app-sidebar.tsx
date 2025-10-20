1966bf20bd1dff5bf6515acbbf6375be57b14aee
Shorno <fb.shorno@gmail.com>
2025-10-21 01:24:26 +0600

    dashboard setup

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,71 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconInnerShadowTop,
+} from "@tabler/icons-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import {ListIcon, ShoppingBagIcon} from "lucide-react";
+import Link from "next/link";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Orders",
+      url: "/admin/dashboard/orders",
+      icon: ShoppingBagIcon,
+    },
+
+    {
+      title: "Products",
+      url: "/admin/dashboard/products",
+      icon: ListIcon,
+    },
+  ],
+
+
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <Link href="/">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-xl font-bold text-green-600">OrganicShop</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
---
+++ components/nav-main.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import {type Icon} from "@tabler/icons-react"
+
+import {
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem, useSidebar,
+} from "@/components/ui/sidebar"
+import {LucideIcon} from "lucide-react";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+
+export function NavMain({
+                            items,
+                        }: {
+    items: {
+        title: string
+        url: string
+        icon?: Icon | LucideIcon
+    }[]
+}) {
+    const {setOpenMobile} = useSidebar()
+
+    const pathname = usePathname();
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-2 mt-8">
+                <SidebarMenu>
+                    {items.map((item) => {
+                        const isActive = pathname === item.url;
+                        console.log(isActive)
+                        return (
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton
+                                    tooltip={item.title}
+                                    asChild
+                                    onClick={() => setOpenMobile(false)}
+                                    isActive={isActive}
+                                    className="flex items-cente gap-3 px-3 h-12 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground data-[active=true]:bg-green-400 data-[active=true]:text-accent-foreground"
+
+                                >
+                                    <Link href={item.url}>
+                                        {item.icon && <item.icon size={20}/>}
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        )
+                    })}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}
