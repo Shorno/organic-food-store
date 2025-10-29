7c58f212f339e61fb3962ba25ee791477cd19f15
Shorno <fb.shorno@gmail.com>
2025-10-30 03:11:16 +0600

    checkout page

--- app/(client)/loading.tsx
+++ app/(client)/loading.tsx
@@ -2,7 +2,7 @@ import {Loader} from "lucide-react";
 
 export default function Loading(){
     return (
-        <div className={"min-h-[calc(100dvh-69px)] w-full flex justify-center items-center"}>
+        <div className={"min-h-[calc(100dvh-130px)] w-full flex justify-center items-center"}>
             <Loader className={"animate-spin"}/>
         </div>
     )
