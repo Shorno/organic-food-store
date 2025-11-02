ccd1b8f2d33fba95ffc7f5ec4d77601aafe9c465
Shorno <fb.shorno@gmail.com>
2025-11-02 06:01:50 +0600

    update payment method

--- app/api/payment/ipn/route.ts
+++ app/api/payment/ipn/route.ts
@@ -1,16 +1,17 @@
-import { NextRequest, NextResponse } from "next/server"
-import { db } from "@/db/config"
-import { order, payment } from "@/db/schema"
-import { eq } from "drizzle-orm"
+import {NextRequest, NextResponse} from "next/server"
+import {db} from "@/db/config"
+import {order, payment} from "@/db/schema"
+import {eq} from "drizzle-orm"
 
 export async function POST(request: NextRequest) {
     try {
         const body = await request.formData()
-        console.log(body)
 
         const status = body.get("status") as string
         const tranId = body.get("tran_id") as string
         const orderId = parseInt(body.get("value_a") as string)
+        // const valId = body.get("val_id") as string
+        const cardIssuer = body.get("card_issuer") as string
 
 
         if (status === "VALID" || status === "VALIDATED") {
@@ -26,14 +27,16 @@ export async function POST(request: NextRequest) {
                 await tx
                     .update(payment)
                     .set({
+                        transactionId: tranId,
+                        paymentMethod: cardIssuer,
                         status: "completed",
                         completedAt: new Date(),
-                        paymentProvider: tranId,
+                        paymentProvider: "sslcommerz",
                     })
                     .where(eq(payment.orderId, orderId))
             })
 
-            return NextResponse.json({ success: true, message: "Payment confirmed" })
+            return NextResponse.json({success: true, message: "Payment confirmed"})
         } else if (status === "FAILED") {
             await db
                 .update(payment)
@@ -43,15 +46,15 @@ export async function POST(request: NextRequest) {
                 })
                 .where(eq(payment.orderId, orderId))
 
-            return NextResponse.json({ success: false, message: "Payment failed" })
+            return NextResponse.json({success: false, message: "Payment failed"})
         }
 
-        return NextResponse.json({ success: false, message: "Unknown status" })
+        return NextResponse.json({success: false, message: "Unknown status"})
     } catch (error) {
         console.error("IPN Error:", error)
         return NextResponse.json(
-            { success: false, error: "Internal server error" },
-            { status: 500 }
+            {success: false, error: "Internal server error"},
+            {status: 500}
         )
     }
 }
